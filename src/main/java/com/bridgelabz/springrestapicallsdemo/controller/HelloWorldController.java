d50fbd4c9a87fcb889c2386a89f6be7a01824e14
Senthil Kumar S A <sasenthilk@gmail.com>
2021-11-01 03:49:41 +0530

    Added: To demonstrate post request method

--- src/main/java/com/bridgelabz/springrestapicallsdemo/controller/HelloWorldController.java
+++ src/main/java/com/bridgelabz/springrestapicallsdemo/controller/HelloWorldController.java
@@ -2,8 +2,11 @@ package com.bridgelabz.springrestapicallsdemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.bridgelabz.springrestapicallsdemo.model.User;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -22,5 +25,10 @@ public class HelloWorldController {
     @GetMapping("/param/{name}")
     public String sayHelloParam(@PathVariable String name) {
         return "Hello " + name + "!";
+    }
+    @PostMapping("/post")
+    public String sayHello(@RequestBody User user) {
+
+        return "Hello " + user.getFirstName() + " " + user.getLastName();
     }
 }
