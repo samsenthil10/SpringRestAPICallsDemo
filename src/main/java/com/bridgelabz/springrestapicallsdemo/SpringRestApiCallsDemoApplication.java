b5b7aa821ecabf8ff3d3c923a52900d78568e51b
Senthil Kumar S A <sasenthilk@gmail.com>
2021-11-01 03:41:28 +0530

    Added: To intialize project structure

---
+++ src/main/java/com/bridgelabz/springrestapicallsdemo/SpringRestApiCallsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.springrestapicallsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiCallsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApiCallsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/springrestapicallsdemo/SpringRestApiCallsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.springrestapicallsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiCallsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
